bad158df340f691f4a8f8907d67c3d29bf1366a9
shenjy <shenjy24@gmail.com>
2022-06-27 00:05:42 +0800

    getUser

--- miniprogram/pages/login/index.js
+++ miniprogram/pages/login/index.js
@@ -1,9 +1,5 @@
 // pages/login/index.js
 Page({
-
-    /**
-     * 页面的初始数据
-     */
     data: {
 
     },
@@ -18,7 +14,7 @@ Page({
                         url: 'http://localhost:18080/auth/code2session',
                         data: {
                             code: res.code
-                          },
+                        },
                         success(r) {
                             console.log("code2session:" + JSON.stringify(r))
                         }
@@ -35,68 +31,16 @@ Page({
 
     checkSession() {
         wx.checkSession({
-            success: (res) => {
+            success() {
                 console.log("session有效")
             },
-            fail: (res) => {
+            fail() {
                 this.login()
             },
         })
     },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad(options) {
+    getUserInfo() {
 
     },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload() {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh() {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom() {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage() {
-
-    }
 })
