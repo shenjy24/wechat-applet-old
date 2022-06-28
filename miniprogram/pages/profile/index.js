d446de0f1bcdeb9739bce185936d916279f330f7
shenjy <shenjy24@gmail.com>
2022-06-28 12:36:48 +0800

    tabBar

--- miniprogram/pages/profile/index.js
+++ miniprogram/pages/profile/index.js
@@ -14,6 +14,7 @@ Page({
     getUserProfile(e) {
         // 推荐使用 wx.getUserProfile 获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认
         // 开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+        console.log("getUserProfile")
         wx.getUserProfile({
           desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
           success: (res) => {
@@ -29,6 +30,7 @@ Page({
 
       getUserInfo(e) {
         // 不推荐使用 getUserInfo 获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
+        console.log("getUserInfo")
         this.setData({
           userInfo: e.detail.userInfo,
           hasUserInfo: true
